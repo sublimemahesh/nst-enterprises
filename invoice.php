7866072d11704e331f7d605617266b0c1613d542
ADinushika <amalidinushika93@gmail.com>
2018-06-21 17:20:24 +0530

    #106 Create Invoice

---
+++ invoice.php
@@ -0,0 +1,252 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+
+        <link href="css/invoice.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+        <div id="wrapper">        
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>N.S.T. ENTERPRISES</h1>
+                    <h4>CLEARING & FORWARDING AGENTS </h4>
+                    <h4>NO. 116B, ST'ANTONY'S MAWATHA, COLOMBO 13. TEL:0112-336796/0117-207250 FAX:0112-388055 </h4>
+                </div>
+            </div>
+
+            <table class="table1">
+                <tr class="tr-border">
+                    <th colspan="4">TAX INVOICE</th>
+
+                </tr>
+                <tr>
+                    <td rowspan="4" class="col-2 text-to" >To. </td>
+                    <td rowspan="4" class="col-3 td-border"></td>
+                    <td class="col-4">Vat Reg No</td>
+                    <td class="col-5"></td>
+
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Invoice No</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Date</td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td>Vat No.</td>
+                    <td class="td-border"></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="td-border"></td>
+                    <td>Job No</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td rowspan="3" class="text-to"> Consignment</td>
+                    <td rowspan="3" class="td-border"></td>
+                    <td>Cleared Date</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+
+                </tr>
+                <tr>
+                    <td>Gross Weight</td>
+                    <td></td>
+
+                </tr>
+                <tr class="tr-border">
+                    <td>Vessel/Flight</td>
+                    <td class="td-border"></td>
+                    <td>Volume</td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td rowspan="4"></td>
+                    <td rowspan="4"></td>
+                    <td rowspan="4" class="td-border"></td>
+                    <td class="td-border1 td-text"><b>Value</b></td>
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td colspan="3" class="td-tax-invoice-total">Tax Invoice Total</td>
+                    <td></td>
+
+                </tr>
+            </table>
+
+            <table class="table1">
+                <tr>
+                    <th colspan="3" class="col-9">Reimbursement</th>
+                    <th colspan="3" class="col-5 th-border">Value</th>
+                </tr>
+
+                <tr>
+                    <td rowspan="8" class="col-2 text-to" >Statutory</td>
+                    <td rowspan="8" class="col-3"></td>
+                    <td  rowspan="8"class="col-4 td-border"></td>
+                    <td class="col-5"></td>
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td></td>        
+                </tr>
+                <tr>
+                    <td colspan="3" class="td-tax-invoice-total">Sub Total</td>
+                    <td></td>
+
+                </tr>
+
+            </table>
+
+            <table class="table1">
+                <tr>
+                    <th colspan="3" class="col-9 th-text">Delivery</th>
+                    <th class="col-5 th-border">Value</th>
+                </tr>
+
+                <tr>
+                    <td rowspan="12" class="col-2 text-to" ></td>
+                    <td rowspan="12" class="col-3"></td>
+                    <td rowspan="12"class="col-4 td-border"></td>
+                    <td class="col-5"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td colspan="3" class="td-tax-invoice-total">Sub Total</td>
+                    <td></td>
+
+                </tr>
+            </table>
+
+            <table class="table-pay">
+
+                <tr>
+                    <td rowspan="3" class="col-2" ></td>
+                    <td rowspan="3" class="col-3 td-border"></td>
+                    <td class="col-4 td-border td-border-top">Payable Amount</td>
+                    <td class="col-5 td-border td-border-top"></td>
+                </tr>
+
+                <tr>
+                    <td class="td-border">Advance</td>
+                    <td class="td-border"></td>
+                </tr>
+                <tr>
+                    <td class="td-border td-border1">Due(Refund)</td>
+                    <td class="td-border td-border1"></td>
+                </tr>
+            </table>
+
+            <table class="table-amount-word">
+                <tr>
+                    <td class="col-2">Amount In Word Rs.</td>
+                    <td colspan="3" class="col-3"></td>
+                    <td colspan="3" class="col-4"></td>
+                    <td colspan="3" class="col-5"></td>
+                </tr>
+            </table>
+            <div>
+                <p><strong>
+                        ALL CHEQUES SHOULD BE DRAWN IN FAVOUR OF 'N.S.T. ENTERPRISES' AND CROSSED A/C PAYEE N.S.T. ENTERPRISES
+                    </strong></p>
+            </div>
+
+        </div>
+
+
+        <!--        <div id="print_button">
+                    <a href="#" class="btn btn-success btn-lg" onClick="myFunction()">
+                        <span class="glyphicon glyphicon-print"></span> Print
+                    </a>
+                </div>-->
+
+
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script>
+
+//            $(document).ready(function () {
+//                myFunction();
+//            });
+
+            function myFunction() {
+                window.print();
+            }
+        </script>
+    </body>
+</html>
